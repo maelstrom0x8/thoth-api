7b0d7ea282177eb2ee9dab62463ad1331b1771f3
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-10 09:24:10 +0100

    Added LogEntry model

--- src/api/models.py
+++ src/api/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+import uuid
 
+
+class LogEntry(models.Model):
+    class Meta:
+        ordering = ['created']
+
+    log_id = models.UUIDField(
+        default=uuid.uuid4, primary_key=True, editable=False)
+    subject = models.CharField(max_length=200, blank=True)
+    body = models.TextField(max_length=100000)
+    created = models.DateTimeField(auto_now_add=True)
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,1 +1,10 @@
 import pytest
+
+from api import models
+
+@pytest.mark.django_db
+def test_entries_have_unique_creation_dates():
+    e1 = models.LogEntry.objects.create(subject='', body='')
+    e2 = models.LogEntry.objects.create(subject='', body='')
+
+    assert e1.created != e2.created
