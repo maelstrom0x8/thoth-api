30b2f3ebb98244815cb6aec725b4039a7e6c2773
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-14 01:49:32 +0100

    Update log entry id

--- src/api/models.py
+++ src/api/models.py
@@ -1,13 +1,17 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-import uuid
+
+
+class User(AbstractUser):
+    pass
 
 
 class LogEntry(models.Model):
     class Meta:
         ordering = ['created']
 
-    log_id = models.UUIDField(
-        default=uuid.uuid4, primary_key=True, editable=False)
+    log_id = models.AutoField(primary_key=True)
     subject = models.CharField(max_length=200, blank=True)
     body = models.TextField(max_length=100000)
     created = models.DateTimeField(auto_now_add=True)
